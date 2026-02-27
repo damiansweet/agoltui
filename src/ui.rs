d2584a91a33c89ef7cfd594130aeb302a154cbc8
Damian Sweet <damiansweet303@gmail.com>
2026-02-27 10:33:52 -0700

    added todo about setting user_input dynamically with clap arguments

--- src/ui.rs
+++ src/ui.rs
@@ -85,6 +85,7 @@ pub fn init_state(cli_input: Args) -> UiState {
 
     let cli_search_term = cli_input.email.clone();
 
+    //TODO put in logic to set user_input dynamically based on clap arguments passed in
     let user_input = UserInput {
         input: cli_search_term.unwrap_or_default(),
         character_index: 0,
